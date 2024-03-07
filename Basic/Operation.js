e62856a2007312c260da3689a486e47e962db5e8
Qaushal <kalariyakaushal7@gmail.com>
2024-03-07 16:10:14 +0530

    understanding Operation.js

---
+++ Basic/Operation.js
@@ -0,0 +1,24 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " kaushal
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //output:12
+// console.log(1 + "2"); //output:12
+// console.log("1" + 2 + 2); //output:122  because of premitive type conversion,here first string is print then compiler automatically prints rest as string
+// console.log(1 + 2 + "2"); //output:32 
+
+// console.log( (3 + 4) * 5 % 3);
+
+
