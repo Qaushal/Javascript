aa08345135703f434ba211291347f1f0363b7ae6
Qaushal <kalariyakaushal7@gmail.com>
2024-03-11 23:58:26 +0530

    Modify the datatype

--- Basic/dataype.js
+++ Basic/dataype.js
@@ -11,3 +11,18 @@
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object ..... interview question that type of null is object
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
