3ed4f64f4ddf528063d0c2b2c9ffadf9d5bf08f5
Qaushal <kalariyakaushal7@gmail.com>
2024-03-15 21:58:03 +0530

    Unterstanding Array part 1

---
+++ Basic/Array.js
@@ -0,0 +1,41 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9) // put 9 at starting index
+// myArr.shift() // remove 9 at starting index
+
+
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join() //chnage arr to string
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+
+//here the main difference between splice and slice is that splice can modify the original array while slice will not
