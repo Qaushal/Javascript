a3f07e14e5afb99de533612d488c0cb089336c22
Qaushal <kalariyakaushal7@gmail.com>
2024-03-29 19:11:29 +0530

    Understanding Control working in js

---
+++ Control/Switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+//NOTE : WHEN WE DO'T WRITE BREAK KEYWORD THEN THE REST OF CODE WILL AUTOMATICALLY PRINT WHENEVER THE KEY MATCHED EXCEPT DEFAULT LINE
