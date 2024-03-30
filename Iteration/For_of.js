754963e00e5ae90dffc75bff38eb559cfcdd74db
Qaushal <kalariyakaushal7@gmail.com>
2024-03-30 18:08:42 +0530

    Understanding For of loop

---
+++ Iteration/For_of.js
@@ -0,0 +1,40 @@
+// for of: used for travesing array
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of  Object.entries(myObject)) {
+//     console.log(key, ':-', value);
+    
+// }  object cannot traverse easily , we need to add some object property to traverse
