e5d55620ae9cecfd1a8515bd899ce87cd5d8e7cc
Qaushal <kalariyakaushal7@gmail.com>
2024-04-08 00:51:01 +0530

    Understanding fucntions

--- Basic/Function.js
+++ Basic/Function.js
@@ -1,31 +1,44 @@
-const user = {
-    username: "kaushal",
-    prices: 199
+// const user = {
+//     username: "kaushal",
+//     prices: 199
+// }
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+// }
+// //passing object externally
+// // handleObject(user) 
+
+// //passing object internally
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+// const myNewArray = [200, 400, 100, 600]
+
+// function returnSecondValue(getArray){
+//     return getArray[1]
+// }
+
+// //passing Array externally
+// // console.log(returnSecondValue(myNewArray));
+
+// //passing Array internally
+// console.log(returnSecondValue([200, 400, 500, 1000]));
+
+//Asyc Funtion
+
+function loops(){
+    let a =0;
+    for (let i=0 ;i<100;i++){
+        a++;
+    }
+    console.log(a);
 }
 
-function handleObject(anyobject){
-    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
-}
-//passing object externally
-// handleObject(user) 
+setTimeout(loops,1000);
 
-//passing object internally
-handleObject({
-    username: "sam",
-    price: 399
-})
-const myNewArray = [200, 400, 100, 600]
-
-function returnSecondValue(getArray){
-    return getArray[1]
-}
-
-//passing Array externally
-// console.log(returnSecondValue(myNewArray));
-
-//passing Array internally
-console.log(returnSecondValue([200, 400, 500, 1000]));
-
+console.log("helo");
 
 //callback function : calling function from another function
 function square(n){
