a6bbe1ebd9406c537411c07fcbd8cae730a4c764
Qaushal <kalariyakaushal7@gmail.com>
2024-05-08 14:45:02 +0530

    Understanding Promise

---
+++ Promise/tempCodeRunnerFile.js
@@ -0,0 +1,20 @@
+new Promise(function(resolve,reject){
+//     console.log("fetching data");
+//     setTimeout(function(){
+//         let err = true;
+//         if(!err){
+//             resolve({username : "kaushal" , id:"21DIT029"})
+//         }
+//         else{
+//             reject('ERROR OCCURES')
+//         }
+//     },2000)
+// }).then((user)=>{
+//     console.log(user);
+//     return user.username;
+// }).then((username)=>{
+//     console.log(username);
+//     return username;
+// }).catch((error)=>{
+//     console.log(error);
+// })
